1db677b74e6077fb2f79ee5bd60b57ddc10949c5
Mindas <mindaugas.lickus@webelevate.ie>
2019-01-04 20:29:03 +0000

    removed vuetify filler

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,10 @@
       sm8
       md6>
       <div class="text-xs-center">
-        <logo/>
-        <vuetify-logo/>
+
       </div>
       <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-card-title class="headline">Welcome!</v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>For more information on Vuetify, check out the <a
@@ -53,13 +52,9 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
   }
 }
 </script>
