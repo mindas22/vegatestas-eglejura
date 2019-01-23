09f4ad9f999579c37410f0138067a54edd0cd475
Mindas <mindaugas.lickus@webelevate.ie>
2019-01-23 12:50:47 +0000

    ru fixed

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  title: 'Vega Testas',
-  description: 'Vega Testas, gydytoja homeopatė Eglė Aukštakalnienė.',
+  title: "Vega Testas",
+  description: "Vega Testas, gydytoja homeopatė Eglė Aukštakalnienė.",
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
-    '/': {
-      lang: 'lt-LT', // this will be set as the lang attribute on <html>
-      title: 'Vega Testas',
-      description: 'Vega testas, gydytoja homeopatė Eglė Aukštakalnienė.'
+    "/": {
+      lang: "lt-LT", // this will be set as the lang attribute on <html>
+      title: "Vega Testas",
+      description: "Vega testas, gydytoja homeopatė Eglė Aukštakalnienė."
     },
-    '/ru/': {
-      lang: 'ru-RU',
-      title: 'Вега тест',
-      description: 'Vue 驱动的静态网站生成器'
+    "/ru/": {
+      lang: "ru-RU",
+      title: "Вега тест",
+      description: "Вегето-резонансное тестирование (вега тест)"
     }
   },
   themeConfig: {
     locales: {
-      '/': {
+      "/": {
         // text for the language dropdown
-        selectText: 'kalba',
+        selectText: "kalba",
         // label for this locale in the language dropdown
-        label: 'lietuvių',
-        // config for Service Worker 
+        label: "lietuvių",
+        // config for Service Worker
         serviceWorker: {
           updatePopup: {
             message: "Puslapis atnaujintas",
@@ -32,18 +32,18 @@ module.exports = {
         // algolia docsearch options for current locale
         algolia: {},
         nav: [
-          { text: 'Apie', link: '/about/' },
-          { text: 'Konsultacijos', link: '/konsultacija/' },
-          { text: 'Klausimai', link: '/klausimai/' }
-        ],
+          { text: "Apie", link: "/about/" },
+          { text: "Konsultacijos", link: "/konsultacija/" },
+          { text: "Klausimai", link: "/klausimai/" }
+        ]
         // sidebar: {
         //   '/': [/* ... */],
         //   '/nested/': [/* ... */]
         // }
       },
-      '/ru/': {
-        selectText: 'язык',
-        label: 'русский',
+      "/ru/": {
+        selectText: "язык",
+        label: "русский",
         serviceWorker: {
           updatePopup: {
             message: "Страница обновлена.",
@@ -51,16 +51,20 @@ module.exports = {
           }
         },
         nav: [
-          { text: 'Вега тест', link: '/ru/' },
-          { text: 'Эгле Аукштакальнене', link: '/ru/about/' }
+          { text: "Вега тест", link: "/ru/" },
+          { text: "Эгле Аукштакальнене", link: "/ru/about/" }
         ],
         algolia: {},
         sidebar: {
-          '/ru/': [/* ... */],
-          '/zh/nested/': [/* ... */]
+          "/ru/": [
+            /* ... */
+          ],
+          "/zh/nested/": [
+            /* ... */
+          ]
         }
       }
-    },
+    }
   },
-  ga: 'UA-131742364-1'
-}
+  ga: "UA-131742364-1"
+};
